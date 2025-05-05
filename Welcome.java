003080b146ad1c6fa879ab448c12cc996d7334bd
mary kenny <marykenny2704@yahoo.com>
2025-05-05 15:43:49 +0200

    docs: ejercicio 1 Welcome

--- Welcome.java
+++ Welcome.java
@@ -1,12 +1,30 @@
 //Inicializa la clase Welcome
 //Crea el método main
-
 //Tarea 1
 //Define el método greeting que imprima en terminal ¡Hola FemCoder!
-//Ejecuta el método greeting 
-
+//Ejecuta el método greeting
 //Tarea 2
 //Define el método greetingCoder que reciba un nombre y que imprima en terminal ¡Hola <name>!
-//Ejecuta el método greetingCoder 
+//Ejecuta el método greetingCoder
 
 //¿Sabes donde definir el método y en dónde lo has de ejecutar? Si no lo sabes vuelve al archivo de Resources.md
+
+
+public class Welcome {
+    static void greeting() {
+        System.out.println("¡Hola FemCoder!");
+    }
+    static void greetingCoder(String name) {
+        System.out.println("¡Hola " + name + "!");
+    }
+
+     public static void main(String args[]) {
+
+        greeting();
+        greetingCoder("Mary");
+
+
+
+
+    }
+}
